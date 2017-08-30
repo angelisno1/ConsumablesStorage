85082d2c07816aa62f3072492f8e72517cb34e65
lyz <angelisno.1@gmail.com>
2017-08-30 17:28:34 +0800

    添加搜索

---
+++ app/src/main/java/com/inteall/consumablesstorage/ui/StockOutAcitivity.java
@@ -0,0 +1,10 @@
+package com.inteall.consumablesstorage.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+
+public class StockOutAcitivity extends AppCompatActivity {
+}
