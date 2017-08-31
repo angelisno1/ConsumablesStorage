f4d43aa2404680e759314a86f319988f34cc0212
lyz <angelisno.1@gmail.com>
2017-08-31 22:42:04 +0800

    product列表

---
+++ app/src/main/java/com/inteall/consumablesstorage/adapter/ApplyFormAdapter.java
@@ -0,0 +1,10 @@
+package com.inteall.consumablesstorage.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by lyz on 2017/8/31.
+ */
+
+public class ApplyFormAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/inteall/consumablesstorage/view/SpaceItemDecoration.java
@@ -0,0 +1,10 @@
+package com.inteall.consumablesstorage.view;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by lyz on 2017/8/31.
+ */
+
+public class SpaceItemDecoration extends RecyclerView.ItemDecoration {
+}
